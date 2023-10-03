17fc98aa65d6b45360409b159c4489227eb04f77
Ishant Bhurani <67356291+ishantbhurani@users.noreply.github.com>
2023-10-03 20:54:05 +0530

    interact with db

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,29 @@
-export default function Home() {
+import { hash } from 'bcrypt'
+import prisma from '@/lib/db'
+
+async function getAllUsers() {
+  const allUsers = await prisma.user.findMany()
+  return allUsers
+}
+
+async function createUser(email: string, password: string, name?: string) {
+  const hashed = await hash(password, 12)
+  const user = await prisma.user.create({
+    data: {
+      name,
+      email,
+      password: hashed,
+    },
+  })
+  return user
+}
+
+export default async function Home() {
+  // const user = await createUser('test@test.com', 'test')
+  // console.log(user)
+
+  const allUsers = await getAllUsers()
+  console.log(allUsers)
+
   return <h1>Home</h1>
 }
